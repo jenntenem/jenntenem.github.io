d5ffce4b0307d102620e2219644cf120b6f2a701
Jenniffer Tenempaguay <jenntenem@gmail.com>
2024-03-21 16:49:12 -0500

    Language en/es Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  integrations: [astroI18next()],
 });

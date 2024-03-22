139c8c05f8a56aa71cf6b1a4a4a34eea6e225df4
Jenniffer Tenempaguay <jenntenem@gmail.com>
2024-03-22 02:11:12 -0500

    fix: Adapter for server output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [astroI18next()],
-  site: "https://jenntenem.github.io"
+  site: "https://jenntenem.github.io",
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
